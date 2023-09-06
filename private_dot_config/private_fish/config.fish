318cab492edc9078543b458107cc83a937717ffd
waterlens <waterlens@waterlens.moe>
2023-09-06 15:44:45 +0800

    in a normal network

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,4 +1,4 @@
-if grep -q -i microsoft /proc/version
+if false && grep -q -i microsoft /proc/version
   set -gx PROXY_PORT 7890
   set -gx WSL_HOST (cat /etc/resolv.conf | grep nameserver | cut -d ' ' -f 2)
   set -gx http_proxy http://{$WSL_HOST}:{$PROXY_PORT}
