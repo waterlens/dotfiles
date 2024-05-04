78235f12f8d25db68038c79709c154cbe2544728
waterlens <waterlens@waterlens.moe>
2024-05-04 16:18:08 +0800

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,10 +10,26 @@ switch (uname)
 case Darwin
   eval (/opt/homebrew/bin/brew shellenv)
   set -gx SDKROOT (xcrun --show-sdk-path)  
+  set -gxa PATH ~/.orbstack/bin
+  set -gxa PATH "~/Library/Application Support/Coursier/bin"
+  set -gx JAVA_HOME (/usr/libexec/java_home -v 11)
 case '*'
 end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    if type -q atuin
+      atuin init fish | source
+    end
 end
+
+if type -q nodeenv
+  source (nodenv init -|psub)
+  set -gxa PATH ~/.nodenv/bin
+end
+if type -q opam
+  eval (opam env)
+end
+
+
