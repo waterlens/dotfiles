9ce709e7240b4e03872b3fa7dab6ad0befa46aa4
waterlens <waterlens@waterlens.moe>
2025-10-18 15:19:19 +0800

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,16 +1,34 @@
+#!/usr/bin/env fish
+
+### Set up common tools
+
+function cuda_init --description "Initialize CUDA path manually"
+    # CUDA
+    set -gxp PATH /usr/local/cuda/bin
+    set -gxp LD_LIBRARY_PATH /usr/local/cuda/lib
+end
+
+function conda_init --description "Initialize conda manually"
+    if type -q conda
+        eval (conda "shell.fish" hook $argv)
+    else
+        echo "Error: conda command not found." >&2
+    end
+end
+
 switch (uname)
     case Darwin
         eval (/opt/homebrew/bin/brew shellenv)
         set -gxp PATH ~/.orbstack/bin
         set -gxp PATH ~/Library/Application\ Support/Coursier/bin
-        # set -gxp PATH /opt/llvm/bin
+        set -gxp PATH /opt/llvm/bin
         set -gxp PATH /opt/mpl/bin
         set -gxp PATH /opt/tinycc/bin
         if type -q xcrun
             set -gx SDKROOT (xcrun --show-sdk-path)
         end
         set -gx JAVA_HOME (/usr/libexec/java_home -v 17)
-    case Linux
+    case '*'
 end
 
 set -gxp PATH ~/.local/bin
@@ -21,10 +39,34 @@ set -gxp PATH ~/.rbenv/bin
 set -gxp PATH ~/.zvm/bin
 set -gxp PATH ~/.zvm/self
 set -gx ZVM_INSTALL ~/.zvm/self
+# Added by LM Studio CLI (lms)
+set -gxp PATH ~/.lmstudio/bin
+# End of LM Studio CLI section
 
-# CUDA
-set -gxp PATH /usr/local/cuda/bin
-set -gxp LD_LIBRARY_PATH /usr/local/cuda/lib
+if type -q moon
+    moon shell-completion --shell fish >~/.config/fish/completions/moon.fish
+end
+if type -q nodenv
+    source (nodenv init -|psub)
+    set -gxp PATH ~/.nodenv/bin
+end
+if type -q opam
+    eval (opam env)
+end
+if type -q rbenv
+    source (rbenv init -|psub)
+end
+if type -q xmake
+    source ~/.xmake/profile
+end
+
+if type -q helix
+    set -gx EDITOR helix
+else if type -q hx
+    set -gx EDITOR hx
+end
+
+### Set up tools in interactive shell mode
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -32,6 +74,7 @@ if status is-interactive
     if type -q atuin
         atuin init fish | source
     end
+
     if test "$TERM_PROGRAM" != vscode
         if type -q zellij
             # Configure auto-attach/exit to your likings (default is off).
@@ -52,6 +95,8 @@ if status is-interactive
     end
 end
 
+### Add abbreviations
+
 abbr -a gcb -- git checkout -b
 abbr -a gfa -- git fetch --all
 abbr -a gpl -- git pull
@@ -61,58 +106,10 @@ abbr -a gcm -- git commit -m
 abbr -a gst -- git status
 abbr -a gc -- git checkout
 abbr -a glg -- git log --graph --abbrev-commit --decorate --format="format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''%C(white)%s%C(reset) %C(dim white)- %an%C(reset)'" --all
-if type -q zellij
-    abbr -a zj -- zellij
-end
-if type -q helix
-    set -gx EDITOR helix
-    abbr -a hx -- helix
-end
-if type -q hx
-    set -gx EDITOR hx
-end
-
-if false
-    if grep -s -q -i microsoft /proc/version
-        set -gx proxy_port 7890
-        set -gx proxy_host (cat /etc/resolv.conf | grep nameserver | cut -d ' ' -f 2)
-    else
-        set -gx proxy_port 7890
-        set -gx proxy_host localhost
-    end
-    set -gx http_proxy http://{$proxy_host}:{$proxy_port}
-    set -gx https_proxy http://{$proxy_host}:{$proxy_port}
-    set -gx all_proxy http://{$proxy_host}:{$proxy_port}
-end
-
-ulimit -n 10240
-
-if type -q moon
-    moon shell-completion --shell fish >~/.config/fish/completions/moon.fish
-end
-if type -q nodenv
-    source (nodenv init -|psub)
-    set -gxp PATH ~/.nodenv/bin
-end
-if type -q opam
-    eval (opam env)
-end
-if type -q rbenv
-    source (rbenv init -|psub)
-end
-if type -q xmake
-    source ~/.xmake/profile
-end
-
-# You are so slow !!!
-function conda_init --description "Initialize conda"
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    if test -f conda
-        eval conda "shell.fish" hook $argv | source
-    end
-    # <<< conda initialize <<<
-end
+abbr -a gcl -- git clone
+abbr -a gd -- git diff
+abbr -a zj -- zellij
+abbr -a hx -- helix
 
 function last_history_item
     echo $history[1]
@@ -121,9 +118,37 @@ function last_history_token
     echo $history[1] | read -t -a tokens
     echo $tokens[-1]
 end
+
 abbr -a !! --position anywhere --function last_history_item
 abbr -a !, --position anywhere --function last_history_token
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/waterlens/.lmstudio/bin
-# End of LM Studio CLI section
+### Set up proxy
+
+function setup_proxy --description "Sets up proxy environment variables based on the OS"
+    set -l proxy_port 7897 # Use a local variable for the port
+
+    switch (uname)
+        case Darwin
+            set -g proxy_host localhost
+        case Linux
+            if grep -s -q -i microsoft /proc/version
+                # WSL: get host IP from /etc/resolv.conf
+                set -g proxy_host (cat /etc/resolv.conf | grep nameserver | cut -d ' ' -f 2)
+            else
+                set -g proxy_host localhost
+            end
+    end
+
+    if set -q proxy_host[1]
+        set -gx http_proxy "http://{$proxy_host}:{$proxy_port}"
+        set -gx https_proxy "http://{$proxy_host}:{$proxy_port}"
+        set -gx all_proxy "http://{$proxy_host}:{$proxy_port}"
+        set -gx no_proxy "127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,172.29.0.0/16,localhost,*.local,*.crashlytics.com,<local>"
+    end
+end
+
+setup_proxy
+
+### Misc
+
+ulimit -n 10240
